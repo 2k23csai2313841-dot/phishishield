d70b791bc5821fe1658332e6b266da283471a3a7
2k23csai2313841-dot <2k23.csai2313841@gmail.com>
2025-12-22 00:09:22 +0530

    Delete routers directory

--- routers/ErrorRouter.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const ErrorRouter = express.Router();
-import { Errorpage404 } from '../controller/errorController.js';
-
-
-ErrorRouter.get('*',Errorpage404 );
-
-export default ErrorRouter;
